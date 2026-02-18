d317cc6a259b898225d4ce514db0d61c22ec4560
Bensoy <koyamobensoy@gmail.com>
2026-02-18 22:37:16 +0800

    Update public index.html folder structure

---
+++ temp-project/src/app.controller.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppController = void 0;
+const common_1 = require("@nestjs/common");
+const app_service_1 = require("./app.service");
+@(0, common_1.Controller)()
+class AppController {
+    appService;
+    constructor(appService) {
+        this.appService = appService;
+    }
+    @(0, common_1.Get)()
+    getHello() {
+        return this.appService.getHello();
+    }
+}
+exports.AppController = AppController;
+//# sourceMappingURL=app.controller.js.map
